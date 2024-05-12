37425dbb75b34fe77f299e93c6bdeeafd8e9c3ba
rishabkhivsara <rishabjain3500@gmail.com>
2024-05-12 16:21:24 +0530

    Add: Dynamic styling

--- src/features/kanban/index.tsx
+++ src/features/kanban/index.tsx
@@ -5,9 +5,22 @@ import Column from "./column";
 import { NewColumn } from "./column/newColumn";
 import { useGetGroupedTasksQuery } from "./column/column.api";
 import SidepeekHusk from "./sidepeek/husk";
+import { useSearchParams } from "react-router-dom";
+import { useToggle } from "@/utils/useToggle";
+import { useEffect } from "react";
+import { cn } from "@/utils/cn";
 
 function Kanban() {
   const { data, isLoading, isError, error } = useGetGroupedTasksQuery();
+  const [isOpen, toggleWidth] = useToggle(false);
+
+  const [searchParams] = useSearchParams();
+  const selectedCard = searchParams.get("selectedCard");
+
+  useEffect(() => {
+    if (selectedCard) toggleWidth(true);
+    else toggleWidth(false);
+  }, [selectedCard, toggleWidth]);
 
   // TODO: Better strategy for these states.
   if (isLoading) return <div>Loading...</div>;
@@ -18,7 +31,14 @@ function Kanban() {
     <div className="flex h-full flex-col gap-4 bg-primary p-4">
       <Header />
 
-      <div className="flex h-full gap-4 overflow-x-auto pr-1">
+      <div
+        className={cn(
+          "flex h-full w-full gap-4 overflow-x-auto pr-1 transition-all duration-200",
+          {
+            "w-[calc(100%-900px)]": isOpen,
+          },
+        )}
+      >
         {data.map((value) => {
           return <Column key={value.id} type={value.title} />;
         })}
