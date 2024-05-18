9d5ca45bec6d457780dd8cd0de80ad99066fe69f
rishabkhivsara <rishabjain3500@gmail.com>
2024-05-18 19:31:07 +0530

    Optimize bundle in prod

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  esbuild: {
-    drop: ["console", "debugger"],
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), tsconfigPaths()],
+    esbuild: {
+      drop: mode === "production" ? ["console", "debugger"] : [],
+    },
+  };
 });
