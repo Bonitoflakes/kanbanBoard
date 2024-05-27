2742f2d4ac2d8b77b21f429dc177057a8e239687
rishabkhivsara <rishabjain3500@gmail.com>
2024-05-27 19:00:42 +0530

    Perf: Optimize re-renders

--- src/utils/useToggle.tsx
+++ src/utils/useToggle.tsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 type returnType = [boolean, (value?: boolean) => void];
 
 export const useToggle = (defaultState = false): returnType => {
   const [toggle, setToggle] = useState<boolean>(defaultState);
 
-  const toggleHandler = (value?: boolean) => {
+  const toggleHandler = useCallback((value?: boolean) => {
     if (value !== undefined) return setToggle(value);
     setToggle((prev) => !prev);
-  };
+  }, []);
 
   return [toggle, toggleHandler];
 };
